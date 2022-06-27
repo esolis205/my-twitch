1e6093f11a00e4156fa4a4a619bba5d989c363ce
Enrique Solis <esolis205@yahoo.com>
2022-06-27 14:45:15 -0700

    Finished Navbar component. Added a functional menu for mobile devices using react-icons/ai .

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,22 @@
 import Link from "next/link";
-import React, {Fragment} from "react";
+import React, { Fragment, useState } from "react";
 import Image from "next/image";
 import Logo from "../public/assets/logo.png";
 import { Menu, Transition } from "@headlessui/react";
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsPerson, BsSearch, BsThreeDotsVertical } from "react-icons/bs";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 function classNames(...classes) {
-    return classes.filter(Boolean).join('');
+  return classes.filter(Boolean).join("");
 }
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="fixed h-14 w-full flex flex-nowrap items-center p-4 bg-[#0e0e10] mb-[2px] z-10 ">
       {/* Left Side */}
@@ -27,8 +34,8 @@ function Navbar() {
         </Link>
         <p className="p-4">Browse</p>
         <div className="p-4">
-          <Menu as='div' className='relative text-left'>
-            <div className='flex'>
+          <Menu as="div" className="relative text-left">
+            <div className="flex">
               <Menu.Button>
                 <BsThreeDotsVertical size={20} />
               </Menu.Button>
@@ -36,24 +43,39 @@ function Navbar() {
 
             <Transition
               as={Fragment}
-              enter='transition ease-out duration-100'
-              enterFrom='transform opacity-0 scale-95'
-              enterTo='transform opacity-100 scale-100'
-              leave='transition ease-in duration-75'
-              leaveFrom='transform opacity-100 scale-100'
-              leaveTo='transform opacity-0 scale-95'
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
             >
-              <Menu.Items className='origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none'>
-                <div className='py-1'>
+              <Menu.Items className="origin-top-right absolute left-0 mt-2 w-56 rounded-md shadow-lg bg-[#0e0e10] ring-1 ring-white ring-opacity-5 focus:outline-none">
+                <div className="py-1 flex flex-col">
                   <Menu.Item>
                     {({ active }) => (
                       <a
-                        href='#'
+                        href="#"
                         className={classNames(
                           active
-                            ? 'bg-gray-500 text-gray-100'
-                            : 'text-gray-200',
-                          'block px-4 py-2 text-sm'
+                            ? "bg-gray-500 text-gray-100"
+                            : "text-gray-200",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        Account
+                      </a>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href="#"
+                        className={classNames(
+                          active
+                            ? "bg-gray-500 text-gray-100"
+                            : "text-gray-200",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Settings
@@ -63,12 +85,12 @@ function Navbar() {
                   <Menu.Item>
                     {({ active }) => (
                       <a
-                        href='#'
+                        href="#"
                         className={classNames(
                           active
-                            ? 'bg-gray-500 text-gray-100'
-                            : 'text-gray-200',
-                          'block px-4 py-2 text-sm'
+                            ? "bg-gray-500 text-gray-100"
+                            : "text-gray-200",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         Support
@@ -78,12 +100,12 @@ function Navbar() {
                   <Menu.Item>
                     {({ active }) => (
                       <a
-                        href='#'
+                        href="#"
                         className={classNames(
                           active
-                            ? 'bg-gray-500 text-gray-100'
-                            : 'text-gray-200',
-                          'block px-4 py-2 text-sm'
+                            ? "bg-gray-500 text-gray-100"
+                            : "text-gray-200",
+                          "block px-4 py-2 text-sm"
                         )}
                       >
                         License
@@ -96,8 +118,62 @@ function Navbar() {
           </Menu>
         </div>
       </div>
+
       {/* Middle */}
+      <div className="hidden md:flex grow-[2] items-center justify-center">
+        <div className="bg-gray-500 text-white flex justify-between items-center max-w-[400px] w-full m-auto p-2 rounded-2xl">
+          <div>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-transparent border-none text-white focus:outline-none"
+            />
+          </div>
+          <div>
+            <BsSearch />
+          </div>
+        </div>
+      </div>
+
       {/* Right Side */}
+      <div className="hidden md:flex grow items-center justify-end">
+        <div className="flex items-center">
+          <Link href="/">
+            <button className="px-4 py-[6px] rounded-lg bg-[#9147ff] mr-2">
+              Account
+            </button>
+          </Link>
+          <BsPerson size={30} />
+        </div>
+      </div>
+      {/* Hamburger Menu */}
+      <div onClick={handleNav} className="block md:hidden z-10 cursor-pointer">
+        {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={
+          nav
+            ? "md:hidden fixed top-0 left-0 w-full h-screen bg-[#0e0e10] flex justify-center items-center ease-in duration-300"
+            : "md:hidden fixed top-[-100%] left-0 w-full h-screen bg-[#0e0e10] flex justify-center items-center ease-in duration-300"
+        }
+      >
+        <ul className="text-center">
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Live Channels</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Top Categories</Link>
+          </li>
+          <li className="p-4 text-3xl font-bold">
+            <Link href="/">Account</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }

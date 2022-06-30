a7b55d7dc08eb31ea4254fb926ca086a54e87e09
Enrique Solis <esolis205@yahoo.com>
2022-06-30 14:10:23 -0700

    Added hover affect to the props { title } and { viewers } from CategoriesItem components.

--- components/CategoriesItem.js
+++ components/CategoriesItem.js
@@ -6,15 +6,15 @@ function CategoriesItem({ img, title, viewers, tag1, tag2 }) {
     <div className="p-2 flex flex-col">
       <Image src={img} alt="/" width="261" height="350" />
       <div>
-          <p className="font-bold">{title}</p>
-          <p className="text-sm text-gray-500 py-[2px]">{viewers} viewers</p>
+          <p className=" cursor-pointer font-bold hover:text-[#9147ff]">{title}</p>
+          <p className=" cursor-pointer text-sm text-gray-500 py-[2px] hover:text-[#9147ff]">{viewers} viewers</p>
           <div className="flex flex-row space-x-2">
             <div>
-                <p className="text-sm bg-gray-700 rounded-full inline-block p-[2px px-3">{tag1}</p>
+                <p className="text-sm bg-gray-700 rounded-full inline-block p-[2px] px-3">{tag1}</p>
             </div>
             <div>
                 {tag2 ? (
-                    <p className="text-sm bg-gray-700 rounded-full inline-block p-[2px px-3">{tag2}</p>
+                    <p className="text-sm bg-gray-700 rounded-full inline-block p-[2px] px-3">{tag2}</p>
                 ) : null}
             </div>
           </div>
